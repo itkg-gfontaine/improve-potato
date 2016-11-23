06760f4b79725391c62107657de3b7b39c0fecce
itkg-gfontaine <guillaume.fontaine@businessdecision.com>
2016-11-23 16:49:37 +0100

    sprint2- isolation de la base de données

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=172.17.0.5;dbname=microcms;charset=utf8', 'root', 'secret');
-$articles = $bdd->query('select * from t_article order by art_id desc');
 
+require 'model.php';
+$articles = getArticles();
 require 'view.php';
