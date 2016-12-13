70373482f62408b405b98f34344510e8c6241eba
itkg-gfontaine <guillaume.fontaine@businessdecision.com>
2016-12-13 10:07:58 +0100

    sprint11-preparation pour production

--- app/config/dev.php
+++ app/config/dev.php
@@ -1,7 +1,18 @@
 <?php
 
-//include the prod configuration
-require __DIR__.'/prod.php';
+// Doctrine (db)
+$app['db.options'] = array(
+	'driver'   => 'pdo_mysql',
+	'charset'  => 'utf8',
+	'host'     => '172.17.0.4',
+	'port'     => '3306',
+	'dbname'   => 'microcms',
+	'user'     => 'root',
+	'password' => 'secret',
+	);
 
 // enable the debug mode
 $app['debug'] = true;
+
+// define log level
+$app['monolog.level'] = 'INFO';
